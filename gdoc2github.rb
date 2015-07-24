48f9830dae4dd9ac7e739d57d335dd5d76000c77
MrOrz <johnsonliang7@gmail.com>
2015-07-24 08:56:49 +0800

    Refactor godoc2github script. Also record authorization_uri and scope data into Redis / init_token.json

--- gdoc2github.rb
+++ gdoc2github.rb
@@ -4,35 +4,47 @@ require 'google/apis/drive_v2'
 require 'google/api_client/client_secrets'
 require 'google/api_client/auth/installed_app'
 
+#
+# 1. Fetch revision from Google Drive
+#
 
-client_secret_options = JSON.parse(File.read('client_secrets.json'))
-
+# Create Google oauth authentication
 # http://www.rubydoc.info/github/google/google-api-ruby-client/Google%2FAPIClient%2FClientSecrets%3Ainitialize
+#
+client_secrets = Google::APIClient::ClientSecrets.new JSON.parse(File.read('client_secrets.json'))
+authorization = client_secrets.to_authorization
+
+# Populate auth data
+#
+auth_options = {}
 if false
   # Get tokens from Redis
-  client_secret_options['installed'].merge! {} #TODO: Some data from redis...
+  authorization.update! {} #TODO: Some data from redis...
 else
   # No tokens found, fetch from initial tokens
-  client_secret_options['installed'].merge! hash_to_auth_options!(JSON.parse(File.read('initial_tokens.json')))
+  authorization.update! hash_to_auth_options!(JSON.parse(File.read('initial_tokens.json')))
 end
 
-client_secrets = Google::APIClient::ClientSecrets.new client_secret_options
+# Update access token if needed
+#
+if authorization.expired?
+  puts "Access token expired, updating..."
+  authorization.fetch_access_token!
 
-authorizaiton = client_secrets.to_authorization
-authorizaiton.update!(
-  :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
-  :redirect_uri => 'urn:ietf:wg:oauth:2.0:oob'
-)
-
-if authorizaiton.expired?
-  authorizaiton.fetch_access_token!
-  puts authorization_to_hash(authorizaiton) #TODO: save to redis
+  puts "Access token updated."
+  puts authorization_to_hash(authorization) #TODO: save to redis
 end
 
+# Fetch revision Data
+#
 drive = Google::Apis::DriveV2::DriveService.new
-drive.authorization = authorizaiton
+drive.authorization = authorization
 files = drive.list_files
 
 files.items.each do |file|
   puts file.title
 end
+
+#
+# 2. Git operations
+#
--- utils.rb
+++ utils.rb
@@ -1,17 +1,19 @@
 
 def authorization_to_hash(authorization)
   hash = {}
-  %w'access_token
-   authorization_uri
-   client_id
-   client_secret
-   expires_in
-   refresh_token
-   token_credential_uri'.each do |var|
+  %w'
+    scope
+    access_token
+    client_id
+    client_secret
+    expires_in
+    refresh_token'.each do |var|
     hash[var] = authorization.instance_variable_get("@#{var}")
   end
-  hash['token_credential_uri'] = hash['token_credential_uri'].to_s
-  hash['authorization_uri'] = hash['authorization_uri'].to_s
+
+  hash['redirect_uri'] = authorization.redirect_uri.to_s
+  hash['token_credential_uri'] = authorization.token_credential_uri.to_s
+  hash['authorization_uri'] = authorization.authorization_uri.to_s
   hash['issued_at'] = authorization.issued_at.to_i
 
   hash
