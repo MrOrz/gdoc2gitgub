2b6891c529411bb37ab21b92712ba036eb53bb04
MrOrz <johnsonliang7@gmail.com>
2015-07-24 20:24:12 +0800

    Redis URL config name change to REDIS_URL

--- lib/storage.rb
+++ lib/storage.rb
@@ -2,7 +2,7 @@ require 'redis'
 require 'json'
 
 class Storage
-  uri = URI.parse ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379"
+  uri = URI.parse ENV["REDIS_URL"] || "redis://127.0.0.1:6379"
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
   def self.google_credential
